770488736e3ed80efa298c002aa6d06e39303870
Auyezkhan Yernar <elenwarface@gmail.com>
2024-06-11 23:47:29 +0500

    +pagereloadfixes+serviceslist+diagram

--- src/pages/CargoPage.jsx
+++ src/pages/CargoPage.jsx
@@ -1,3 +1,11 @@
+import adv_yellow from "../assets/adv_yellow.png";
+import adv_green from "../assets/adv_green.png";
+import adv_pink from "../assets/adv_pink.png";
+import adv_purple from "../assets/adv_purple.png";
+import adv_red from "../assets/adv_red.png";
+import cust_risks from "../assets/cust_risks.png";
+import deliv_econ from "../assets/deliv_econ.png";
+
 import { AdvantagesDiagram } from "../ReusableComponents/AdvantagesDiagram";
 import { IntroSection } from "../sections/IntroSection";
 import { MainSection } from "../sections/MainSection";
@@ -18,7 +26,50 @@ export function CargoPage() {
       ></MainSection>
       <div className="main_2">
         <h2>Наши преимущества</h2>
-        <AdvantagesDiagram></AdvantagesDiagram>
+        <div className="adv_group">
+          <div className="adv_group_piece ">
+            <div className="adv_group_piece_textbox">
+              <h4>Сокращение расходов на логистику</h4>
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>{" "}
+            <img src={adv_yellow} alt="" />
+          </div>
+          <div className="adv_group_piece even">
+            <div className="adv_group_piece_textbox">
+              <h4>Экономия на доставке</h4>
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>{" "}
+            <div className="adv_group_piece_imgs">
+              <img src={adv_purple} alt="" />
+
+              <img className="diagram_img" src={deliv_econ} alt="" />
+            </div>
+          </div>
+          <div className="adv_group_piece">
+            <div className="adv_group_piece_textbox">
+              <h4>Снижение таможенных рисков</h4>
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>{" "}
+            <img src={adv_green} alt="" />
+          </div>
+          <div className="adv_group_piece even">
+            <div className="adv_group_piece_textbox">
+              <h4>Уменьшение затрат на хранение</h4>
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>{" "}
+            <div className="adv_group_piece_imgs">
+              <img src={adv_red} alt="" />
+              <img className="diagram_img" src={cust_risks} alt="" />
+            </div>
+          </div>
+          <div className="adv_group_piece">
+            <div className="adv_group_piece_textbox">
+              <h4>Консолидация товаров</h4>
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>{" "}
+            <img src={adv_pink} alt="" />
+          </div>
+        </div>
       </div>
     </>
   );
--- src/sections/ServicesListSection.jsx
+++ src/sections/ServicesListSection.jsx
@@ -7,21 +7,30 @@ import services_cargo from "../assets/services_cargo.png";
 import services_cont from "../assets/services_cont.png";
 import services_multi from "../assets/services_multi.png";
 import services_vagon from "../assets/services_vagon.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 export function ServicesListSection() {
+  const navigate = useNavigate();
+
+  function handleNavigation(path) {
+    navigate(path);
+    window.scrollTo(0, 0);
+    window.location.reload();
+  }
   return (
     <div className="main_3">
       <h2>Наши услуги</h2>
       <img className="main_3-bgimage" src={main3_img} alt="" />
       <div className="services_block">
         <div className="services_column services_column-left">
-          <div className="services_column_piece">
+          <div
+            className="services_column_piece"
+            onClick={() => {
+              handleNavigation("truck");
+            }}
+          >
             <img src={services_truck} alt="" />
             <div className="services_column_piece_textbox">
-              <Link to={"truck"}>
-                {" "}
-                <h4>FTL из Турции, Европы и Китая</h4>
-              </Link>
+              <h4>FTL из Турции, Европы и Китая</h4>
               <p>
                 Sprint Logistics поможет Вам забрать Ваш груз из любой точки
                 Европы, России, Турции, Китая и доставить на полной фуре в
@@ -29,13 +38,16 @@ export function ServicesListSection() {
               </p>
             </div>
           </div>
-          <div className="services_column_piece">
+          <div
+            className="services_column_piece"
+            onClick={() => {
+              handleNavigation("cargo");
+            }}
+          >
             <img src={services_cargo} alt="" />
             <div className="services_column_piece_textbox">
-              <Link to={"cargo"}>
-                {" "}
-                <h4>Доставка сборных грузов из Китая, Турции и Европы</h4>
-              </Link>
+              <h4>Доставка сборных грузов из Китая, Турции и Европы</h4>
+
               <p>
                 Sprint Logistics поможет Вам забрать Ваш груз из любой точки
                 Китая и Турции и доставить Авто и ЖД консолидацией в Казахстан
@@ -43,13 +55,15 @@ export function ServicesListSection() {
               </p>
             </div>
           </div>
-          <div className="services_column_piece">
+          <div
+            className="services_column_piece"
+            onClick={() => {
+              handleNavigation("cont");
+            }}
+          >
             <img src={services_cont} alt="" />
             <div className="services_column_piece_textbox">
-              <Link to={"cont"}>
-                {" "}
-                <h4>Контейнерные перевозки из Китая</h4>
-              </Link>
+              <h4>Контейнерные перевозки из Китая</h4>
               <p>
                 Доверьте контейнерные перевозки Sprint Logistics - мы доставим
                 Ваш груз из Турции и Китая в Казахстан быстро, надежно и по
@@ -57,13 +71,15 @@ export function ServicesListSection() {
               </p>
             </div>
           </div>
-          <div className="services_column_piece">
+          <div
+            className="services_column_piece"
+            onClick={() => {
+              handleNavigation("samosval");
+            }}
+          >
             <img src={services_samosval} alt="" />
             <div className="services_column_piece_textbox">
-              <Link to={"samosval"}>
-                {" "}
-                <h4>Перевозка спецтехники и негабаритных грузов</h4>
-              </Link>
+              <h4>Перевозка спецтехники и негабаритных грузов</h4>
               <p>
                 Sprint Logistics поможет Вам в организации перевозки специальных
                 грузов по территории Казахстана
@@ -72,13 +88,15 @@ export function ServicesListSection() {
           </div>
         </div>
         <div className="services_column services_column-right">
-          <div className="services_column_piece">
+          <div
+            className="services_column_piece"
+            onClick={() => {
+              handleNavigation("multi");
+            }}
+          >
             <img src={services_multi} alt="" />
             <div className="services_column_piece_textbox">
-              <Link to={"multi"}>
-                {" "}
-                <h4>Мультимодальные перевозки</h4>
-              </Link>
+              <h4>Мультимодальные перевозки</h4>
               <p>
                 Команда Sprint Logistics поможет Вам с перевозкой грузов разными
                 видами транспорта АВТО, Ж/Д, АВИА, Морем по всему миру в
@@ -86,26 +104,30 @@ export function ServicesListSection() {
               </p>
             </div>
           </div>
-          <div className="services_column_piece">
+          <div
+            className="services_column_piece"
+            onClick={() => {
+              handleNavigation("vagon");
+            }}
+          >
             <img src={services_vagon} alt="" />
             <div className="services_column_piece_textbox">
-              <Link to={"vagon"}>
-                {" "}
-                <h4>Железнодорожные перевозки</h4>
-              </Link>
+              <h4>Железнодорожные перевозки</h4>
               <p>
                 Предоставление подвижного состава для доставки Вашего груза по
                 Казахстану, в Европу и по всей Средней Азии!
               </p>
             </div>
           </div>
-          <div className="services_column_piece">
+          <div
+            className="services_column_piece"
+            onClick={() => {
+              handleNavigation("avia");
+            }}
+          >
             <img src={services_avia} alt="" />
             <div className="services_column_piece_textbox">
-              <Link to={"avia"}>
-                {" "}
-                <h4>Авиадоставка по всему миру</h4>
-              </Link>
+              <h4>Авиадоставка по всему миру</h4>
               <p>
                 SPRINT LOGISTICS поможет Вам забрать Ваш груз из любой точки
                 мира и доставить авиатранспортом в Казахстан по самой низкой
@@ -113,13 +135,17 @@ export function ServicesListSection() {
               </p>
             </div>
           </div>
-          <div className="services_column_piece">
+          <div
+            className="services_column_piece"
+            onClick={() => {
+              handleNavigation("serv");
+            }}
+          >
             <img src={services_serv} alt="" />
             <div className="services_column_piece_textbox">
-              <Link to={"serv"}>
-                {" "}
-                <h4>Дополнительные услуги в процессе перевозки</h4>
-              </Link>
+              <h4 onClick={() => {}}>
+                Дополнительные услуги в процессе перевозки
+              </h4>
               <p>
                 Sprint Logistics предоставляет услуги по страхованию груза,
                 брокерским услугам, а также услуги по охранному сопровождению
